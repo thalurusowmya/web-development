d6e618cd7ae28aefb34c0caf3630df118d8a9d50
thalurusowmya <sowmyathaluru@gmail.com>
2024-12-24 22:10:14 +0530

    Add files via upload

---
+++ To-Do-List using JS/todo.js
@@ -0,0 +1,39 @@
+let btn = document.querySelector("button");
+let ul=document.querySelector("ul");
+let inp = document.querySelector("input");
+
+btn.addEventListener("click", function(){
+    let item= document.createElement("li");
+    item.innerText=inp.value;
+
+    let delbtn= document.createElement("button");
+    delbtn.innerText="delete";
+    delbtn.classList.add("delete");
+
+    item.appendChild(delbtn);
+    ul.appendChild(item);
+    // console.log(inp.value);
+    inp.value="";
+});
+
+ul.addEventListener("click", function(event){
+    // console.log(event.target);
+    if(event.target.nodeName == "BUTTON"){
+        let listitem = event.target.parentElement;
+        listitem.remove();
+        // console.log(listitem);
+        console.log("deleted");
+    }
+    // console.log("button clicked");
+});
+
+
+// let delbtns=document.querySelectorAll(".delete");
+// for (delbtn of delbtns){
+//     delbtn.addEventListener("click", function(){
+//         // console.log("element deleted");
+//         let par = this.parentElement;
+//         console.log(par);
+//         par.remove();
+//     });
+// }
